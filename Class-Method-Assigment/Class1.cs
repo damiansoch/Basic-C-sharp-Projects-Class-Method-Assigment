776b6f42ab09ef90afa40156a6cb7715a407f5ef
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-17 07:39:39 +0100

    Add project files.

---
+++ Class-Method-Assigment/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Method_Assigment
+{
+    public class Class1
+    {
+        public void Div(int number)
+        {
+            double result = number / 2.00;
+
+            Console.WriteLine("1/2 of {0}" + " is {1}", number, result);
+        }
+
+        public double Div2(int number)
+        {
+            double result = number / 2.00;
+
+            return result;
+        }
+
+        public double Div2(int numberOne, int numberTwo)
+        {
+            double result = numberOne / numberTwo;
+
+            return result;
+        }
+    }
+}
---
+++ Class-Method-Assigment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Class_Method_Assigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            Console.WriteLine("Please enter the whole number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            class1.Div(number);
+
+
+        }
+    }
+}
